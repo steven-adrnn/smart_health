ce45e2b0a83c058b18458e6847943c0171a570bd
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-12-27 20:57:31 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-// app/page.tsx
-// import Header from '@/components/Header';
 import { Hero } from '@/components/Hero';
 import Categories from '@/components/Categories';
 import { Footer } from '@/components/Footer';
-import { supabase } from '@/lib/supabaseClient';
 
-const HomePage = async () => {
-    const { data: { session } } = await supabase.auth.getSession();
-
+const HomePage = () => {
     return (
         <div>
-            {/* <Header session={session} /> */}
             <Hero />
             <Categories />
             <Footer />
