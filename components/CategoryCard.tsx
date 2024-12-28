faf972c0d5183a5706e321fb4b9048ac6c706dd4
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-12-29 05:20:04 +0700

    .

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -3,21 +3,23 @@ import Image from 'next/image';
 
 interface CategoryCardProps {
     name: string;
-    image: string; // Nama file gambar kategori
+    image: string;
 }
 
 const CategoryCard: React.FC<CategoryCardProps> = ({ name, image }) => {
     return (
-        <Link href={`/category/${name.toLowerCase()}`}>
-            <div className="flex flex-col items-center justify-center border rounded-lg shadow-md p-4 cursor-pointer hover:shadow-lg transition-shadow">
-                <Image 
-                    src={`/${image}`} // Menggunakan path relatif ke folder public
-                    alt={name}
-                    width={200} // Sesuaikan ukuran sesuai kebutuhan
-                    height={500} // Sesuaikan ukuran sesuai kebutuhan
-                    className="w-full h-32 object-cover rounded-md mb-2"
-                />
-                <h3 className="text-lg font-semibold">{name}</h3>
+        <Link href={`/category/${name.toLowerCase()}`} className="block">
+            <div className="flex flex-col items-center justify-center border rounded-lg shadow-md p-4 cursor-pointer hover:shadow-lg transition-shadow h-full">
+                <div className="w-full aspect-video mb-2 relative"> {/* Gunakan aspect-video untuk rasio 16:9 */}
+                    <Image 
+                        src={'/${image}'} 
+                        alt={name}
+                        fill // Gunakan fill untuk memenuhi container
+                        className="object-cover rounded-md"
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    />
+                </div>
+                <h3 className="text-lg font-semibold text-center mt-2">{name}</h3>
             </div>
         </Link>
     );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,7 @@
 export function Footer() {
   return (
     <footer className="bg-green-500 text-white py-8 px-4">
-      <div className="container mx-auto mt-8 pt-8 border-t border-green-400 text-center">
         <p>&copy; 2024 Smart-Health. All rights reserved.</p>
-      </div>
     </footer>
   )
 }
