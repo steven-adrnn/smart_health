8ec6c5c4d7ff58f9ea0e4837feea47a102d27406
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-12-28 19:44:56 +0700

    .

--- lib/supabaseServer.ts
+++
@@ -1,8 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { Database } from './database.types';
-
-export const createServerSupabaseClient = () => 
-  createServerComponentClient<Database>({ 
-    cookies 
-  });
