60906b73286e9d7acf78efd76a12dd672934723d
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-12-28 23:17:15 +0700

    .

--- next.config.js
+++ next.config.js
@@ -15,20 +15,6 @@ const nextConfig = {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
-    
-    async headers() {
-      return [
-        {
-          source: '/auth/callback',
-          headers: [
-            {
-              key: 'Cross-Origin-Opener-Policy',
-              value: 'same-origin-allow-popups'
-            }
-          ]
-        }
-      ];
-    },
   }
   
   module.exports = nextConfig
