0be87a3b2fbe42bc588d8a6da0dac24c8bc7c0da
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-01-08 04:17:38 +0700

    .

--- next.config.js
+++ next.config.js
@@ -47,7 +47,7 @@ const nextConfig = {
         {
           source: '/api/recipes',
           headers: [
-            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Origin', value: 'https://fit-kitchen-frontend-tst.vercel.app' },
             { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' },
             { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization, X-API-Key' },
           ],
