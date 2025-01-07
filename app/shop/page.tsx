1e8eed9791320c68886a06a6874e88f7b9cec0dc
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-01-08 03:41:29 +0700

    .

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -14,7 +14,7 @@ import {
     SelectTrigger, 
     SelectValue 
 } from '@/components/ui/select';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 
 // Definisi tipe produk yang lebih spesifik
