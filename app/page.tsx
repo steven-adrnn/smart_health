d90d1d4bf41d0969afb122a96fa752d8b3088583
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-01-09 21:31:07 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function HomePage () {
                 }}
             />
             <Script
-                src="https://spotify-bot.azurewebsites.net/api/chat"
+                src="https://spotify-bot.azurewebsites.net/static/js/widget-loader.js"
                 strategy="afterInteractive"
             />
             <Footer />
